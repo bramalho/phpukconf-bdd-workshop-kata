caa8f110f8f4ede9a4e0180287fcfac15783408c
bruno.ramalho <bruno.ramalho@olx.com>
2018-02-14 11:40:41 +0000

    cleanup

--- src/RomanNumber.php
+++ src/RomanNumber.php
@@ -2,23 +2,24 @@
 
 class RomanNumber
 {
+    /** @var array $map */
+    private $map  = [
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I',
+    ];
+
     /**
      * @param int $number
      * @return null|string
      */
     public function convert(int $number)
     {
-        $map = [
-            10 => 'X',
-            9 => 'IX',
-            5 => 'V',
-            4 => 'IV',
-            1 => 'I',
-        ];
         $romanNumber = '';
-
         while ($number > 0) {
-            foreach ($map as $int => $roman) {
+            foreach ($this->map as $int => $roman) {
                 if($number >= $int) {
                     $number -= $int;
                     $romanNumber .= $roman;
